f131ebe0753a84a55f906790c17743883694faf3
Shohei Asogawa <uken.shohei@gmail.com>
2026-01-06 13:10:35 +0900

    fix: 譛ｪ菴ｿ逕ｨ縺ｮuseEffect繧､繝ｳ繝昴・繝医ｒ蜑企勁縺励※繝薙Ν繝峨お繝ｩ繝ｼ繧剃ｿｮ豁｣

--- src/pages/StoreSearchView.tsx
+++ src/pages/StoreSearchView.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Navbar } from '../components/Navbar';
 import { Icon } from '../components/Icon';
 import { callGemini } from '../lib/gemini';
