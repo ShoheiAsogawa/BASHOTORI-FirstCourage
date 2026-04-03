b225f833bf960d8f904fe0ec0ecbb04e0dcc9280
ShoheiAsogawa <uken.shohei@gmail.com>
2026-04-03 11:43:13 +0900

    fix: add base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   root: __dirname,
+    base: process.env.VITE_BASE_PATH_OVERRIDE ?? '/BASHOTORI/',
   plugins: [react()],
   resolve: {
     alias: {
